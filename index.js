103b7731333ae5f72f115cf665f2ee0a5f729fd1
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-01-23 11:29:54 +0000

    Create basic math funtions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
+
+
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -12,4 +18,11 @@ function multiply(n1, n2) {
 
 function divide(n1, n2) {
     return n1 / n2;
+}
+
+function operate(n1, operator, n2) {
+    if (operator === '+') return add(n1, n2);
+    else if (operator === '-') return subtract(n1, n2);
+    else if (operator === 'x') return multiply(n1, n2);
+    else if (operator === '/') return divide(n1, n2);
 }
